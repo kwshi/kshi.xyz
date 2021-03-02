e5bc93cf2c7fd0611ad5ddd1d37172f532713ce8
Kye Shi <shi.kye@gmail.com>
2021-03-01 16:40:04 -0800

    add loading slider to title bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,8 +1,66 @@
 <script>
+  import {stores} from '@sapper/app';
+
   export let segment: string | undefined;
+
+  const LOADING_ACCEL = 8e-6;
+  const LOADING_TAU = .2e3;
+  const LOADING_MAX = 2/3;
+
+  let pos = 0;
+  let showLoad = false;
+
+  const {preloading} = stores();
+
+  const loadingStart = () => {
+    const start = new Date();
+    showLoad = true;
+
+    const frame = () => {
+      const t = new Date() - start;
+
+      if (!$preloading) {
+        loadingCleanup();
+        return;
+      } 
+
+      pos = (1 - LOADING_TAU/(t + LOADING_TAU)) * LOADING_MAX;
+      requestAnimationFrame(frame);
+    }
+
+    requestAnimationFrame(frame);
+  };
+
+  const loadingCleanup = () => { 
+    const start = new Date();
+
+    const init = pos;
+    const speed = Math.pow(LOADING_MAX - init, 2) / LOADING_MAX / LOADING_TAU;
+
+    const frame = () => {
+      const t = new Date() - start;
+
+      if (pos >= 1) {
+        setTimeout(() => {
+          showLoad = false; 
+          setTimeout(() => pos = 0, 300);
+        }, 75);
+        return;
+      }
+
+      pos = init + speed * t + LOADING_ACCEL/2 * t*t;
+      requestAnimationFrame(frame);
+    };
+
+    requestAnimationFrame(frame);
+  }
+
+  $: if ($preloading) loadingStart();
+
 </script>
 
 <nav>
+  <div class='loading' style='width: {pos * 100}%;' class:shown={showLoad}/>
   <a
     href="/"
     title="Ha, get it?  It&rsquo;s funny because my last name is &ldquo;Shi&rdquo;."
@@ -19,11 +77,27 @@
 <style>
   nav {
     @apply flex flex-row bg-cyan-900 transition-colors;
+    overflow: hidden;
+
+    & > .loading {
+      @apply 
+        absolute left-0 top-0 bottom-0
+        bg-gradient-to-r from-cyan-700 to-cyan-600
+        opacity-0
+        transition-opacity
+        duration-300 
+        ease-linear;
+      
+      &.shown {
+        @apply transition-none opacity-100;
+      }
+    }
+
     & > a {
-      @apply px-4 py-2 text-cyan-50;
+      @apply px-4 py-2 text-cyan-50 z-10;
       &[href="/"] { @apply font-bold; }
       &:hover {
-        @apply bg-cyan-700 text-white;
+        @apply bg-opacity-50 bg-cyan-300 text-white;
       }
     }
   }
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Nav from "@/components/Nav.svelte";
+  import Loading from "@/components/Loading.svelte";
   import { stores } from "@sapper/app";
 
   export let segment: string | undefined;
