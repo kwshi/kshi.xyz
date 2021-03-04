0c7adbdf3c5390b988bfe18a7db5d5173f47a953
Kye Shi <shi.kye@gmail.com>
2021-03-04 10:29:28 -0800

    create anchor & mdsvex post layout components

---
+++ src/components/Anchor.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { scrollto, setGlobalOptions } from "svelte-scrollto";
+
+  export let rel: string | undefined = undefined;
+  export let href: string;
+
+  const hash = href.match("#[^#]+$")?.[0];
+
+  setGlobalOptions({ duration: 300 });
+</script>
+
+{#if hash}
+  <a {href} {rel} class={$$props.class} use:scrollto={hash}>
+    <slot />
+  </a>
+{:else}
+  <a {href} {rel} class={$$props.class}>
+    <slot />
+  </a>
+{/if}
---
+++ src/components/Mdsvex.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+  import Anchor from "./Anchor.svelte";
+
+  const a = Anchor;
+  export { a };
+</script>
+
+<slot />
