ab3a110add8e900c00f8fd752b0f13a8258b8c5a
Kye Shi <shi.kye@gmail.com>
2022-03-19 14:14:11 -0700

    live-updating (once an hour) age counter

--- src/lib/age.ts
+++ src/lib/age.ts
@@ -1,5 +1,13 @@
+import { readable } from "svelte/store";
+
 // because i can :)
-const now = new Date();
-const year = now.getFullYear();
-const birthday = new Date(year, 8, 13);
-export const age = year - 1998 - +(now < birthday);
+const computeAge = (date: Date): number => {
+  const year = date.getFullYear();
+  const birthday = new Date(year, 8, 13); // month is 0-indexed, for some reason
+  return year - 1998 - +(date < birthday);
+};
+
+export const age = readable(computeAge(new Date()), (set) => {
+  const interval = setInterval(() => set(computeAge(new Date())), 3600e3);
+  return () => clearInterval(interval);
+});
